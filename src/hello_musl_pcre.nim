c48429fcb431204d8cc6da6eadc9742141376d47
Kaushal Modi <kaushal.modi@gmail.com>
2019-01-15 17:49:12 -0500

    Prepare for a minimum reproducible example for Nim # 10030

--- src/hello_musl_pcre.nim
+++
@@ -1,7 +0,0 @@
-import re
-
-let
-  s = "Hello, World!"
-  regex = re"H...."
-echo(s)
-echo s.replace(regex, "Bye")
--- src/hello_musl_ssl.nim
+++
@@ -1,7 +0,0 @@
-import httpclient, json
-
-var client = newHttpClient()
-let jObj = client.getContent("https://scripter.co/jf2feed.json").parseJson()
-echo jObj["author"]
-echo jObj["author"].pretty()
-echo jObj["author"]["name"]
